b11f1ac44ff0fe4b6bcd687044803e6e261ca15c
Gregory Hanson <astronautg117@gmail.com>
2017-11-22 00:26:04 -0500

    Added mac project

---
+++ SpaceCamp_MAC/Components/UnitMover.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xna.Framework;
+using Nez;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceCamp.Components
+{
+    public class UnitMover : Component, IUpdatable
+    {
+        public float Speed { get; set;}
+        public Vector2 Direction { get; set; }
+
+        public UnitMover()
+        {
+            Speed = 0;
+            Direction = Vector2.Zero;
+        }
+
+        public void update()
+        {
+            entity.position += Direction * Speed * Time.deltaTime;
+        }
+    }
+}
