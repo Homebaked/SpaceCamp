19a47e4c4c077eb6504170be73aa15ae4b4fda7e
Gregory Hanson <astronautg117@gmail.com>
2017-11-23 11:21:25 -0500

    Added Grid scene

--- SpaceCamp/Scenes/Grid.cs
+++ SpaceCamp/Scenes/Grid.cs
@@ -1,10 +1,34 @@
 ﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Nez;
 namespace SpaceCamp.Scenes
 {
-    public class Grid
+    public class Grid : Scene
     {
-        public Grid()
-        {
+        List<List<GridSquare>> squares;
+        int width, height, squareSize;
+
+        public Entity selectedEntity { get; private set; }
+
+        public Grid(int width, int height, int squareSize, Color? background = null) : base() {
+            if (background.HasValue) { base.clearColor = background.Value; }
+            base.addRenderer(new DefaultRenderer());
+
+            this.width = width;
+            this.height = height;
+            this.squareSize = squareSize;
+
+            squares = new List<List<GridSquare>>();
+            for (int i = 0; i < width; i++) {
+                List<GridSquare> column = new List<GridSquare>();
+                for (int j = 0; j < height; j++) {
+                    column.Add(new GridSquare());
+                }
+                squares.Add(column);
+            }
         }
     }
+
+    public struct GridSquare { };
 }
