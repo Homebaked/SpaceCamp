518606e0fdc646c48ace87a1488a570c2cc043da
Gregory Hanson <astronautg117@gmail.com>
2017-11-24 17:49:18 -0500

    Added AddUnit and AddBuilding methods to Grid

--- SpaceCamp/Scenes/Grid.cs
+++ SpaceCamp/Scenes/Grid.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using Nez;
+using SpaceCamp.Entities;
+
 namespace SpaceCamp.Scenes
 {
     public class Grid : Scene
     {
         List<List<GridSquare>> squares;
-        int width, height, squareSize;
+        int width, height;
+
+        public readonly int squareSize;
 
         public Entity selectedEntity { get; private set; }
 
@@ -27,6 +32,18 @@ namespace SpaceCamp.Scenes
                 }
                 squares.Add(column);
             }
+        }
+
+        public Unit AddUnit(string name, Texture2D texture, float speed) {
+            Unit unit = new Unit(name, texture, speed, this);
+            this.addEntity(unit);
+            return unit;
+        }
+
+        public Building AddBuilding(string name, Texture2D texture, int width, int height) {
+            Building building = new Building(name, texture, width, height, this);
+            this.addEntity(building);
+            return building;
         }
     }
 
