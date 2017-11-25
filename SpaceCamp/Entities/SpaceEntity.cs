87bd403b5ce94e1da1f781503e44bd849146106d
Gregory Hanson <astronautg117@gmail.com>
2017-11-25 11:45:06 -0500

    Doesn't work...

--- SpaceCamp/Entities/SpaceEntity.cs
+++ SpaceCamp/Entities/SpaceEntity.cs
@@ -25,14 +25,16 @@ namespace SpaceCamp.Entities
             Height = height;
 
             //Scale the sprite so that it fits the grid size of the entity.
-            int scale = (int)calculateScale(texture.Width, 
+            float scale = calculateScale(texture.Width, 
                                             texture.Height, 
                                             Width * squareSize, 
                                             Height * squareSize);
+            int subTextureWidth = (int)(texture.Width * scale);
+            int subTextureHeight = (int)(texture.Height * scale);
             Rectangle rect = new Rectangle(0, 
                                            0, 
-                                           scale * texture.Width, 
-                                           scale * texture.Height);
+                                           subTextureWidth, 
+                                           subTextureHeight);
             this.addComponent(new Sprite(new Subtexture(texture, rect)));
         }
 
@@ -43,11 +45,11 @@ namespace SpaceCamp.Entities
             float widthScale = 1, heightScale = 1;
 
             if (width > maxWidth) {
-                widthScale = width / maxWidth;
+                widthScale = (float)maxWidth / width;
             }
 
             if (height > maxHeight) {
-                heightScale = height / maxHeight;
+                heightScale = (float)maxHeight / height;
             }
 
             if (widthScale < 1 && widthScale < heightScale) {
